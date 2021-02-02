7e5596946311acc6ad6120e2d4e04108181fd390
sagarkhanapurkar <sagarkhanapurkar@gmail.com>
2021-02-02 17:39:19 -0500

    Create Mathx.java

---
+++ Mathx.java
@@ -0,0 +1,27 @@
+import java.util.*;  
+class Mathx   
+{  
+
+    public static void main(String[] args)  
+
+
+{  
+
+
+    Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+
+    System.out.print("How many pages does the book has?");  
+
+    int numberOfPages= sc.nextInt();  
+
+    System.out.print("How many pages can you read per day?");  
+
+    int readingRate= sc.nextInt();  
+
+    int d = numberOfPages/readingRate;  
+
+    System.out.println("It will take " + d + " days to finish the book" );  
+
+}  
+
+}  
